8c6e07b3b4dab98d9fa853fc2b1998909d976337
Guan <kuanui21@gmail.com>
2022-04-20 12:05:04 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const validUrl = require('valid-url')
 
-const urlModel = require('./models/urlModel')
-const generateShortUrl = require('./generate_shortUrl')
+const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
@@ -14,47 +12,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  const url = req.body.url
-
-  if (validUrl.isUri(url)) {
-    urlModel.find({ url })
-      .lean()
-      .then(longUrl => {
-        const filterUrl = longUrl.filter(InputUrl => InputUrl.url.includes(url))
-        const shortUrl = generateShortUrl()
-
-        if (filterUrl.length > 0) {
-          const filterShortUrl = filterUrl[0].short_url
-          res.render('index', { isSomeUrl: "true", url, shortUrl: filterShortUrl })
-        } else {
-          return urlModel.create({ url, short_url: shortUrl })
-            .then(() => res.render('show', { url, shortUrl }))
-            .catch(error => console.log(error))
-        }
-      })
-  } else {
-    res.render('index', { isUrl: "false" })
-  }
-})
-
-app.get("/:InputShortUrl", (req, res) => {
-  const InputShortUrl = req.params.InputShortUrl
-  console.log(InputShortUrl)
-  urlModel.findOne({ short_url: InputShortUrl })
-    .then(data => {
-      if (!data) {
-        return res.render('index', { isError: "true", InputShortUrl })
-      }
-      res.redirect(data.url)
-    })
-    .catch(error => console.error(error))
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express App is running on http://localhost:${port}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const validUrl = require('valid-url')
+
+const urlModel = require('../../models/urlModel')
+const generateShortUrl = require('../../generate_shortUrl')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const url = req.body.url
+
+  if (validUrl.isUri(url)) {  // 用 validUrl 偵測輸入網址是否為 url
+    urlModel.find({ url })    // 是 url 的話，搜尋資料庫是否有重複的 url
+      .lean()
+      .then(longUrl => {
+        const filterUrl = longUrl.filter(InputUrl => InputUrl.url.includes(url))
+        const shortUrl = generateShortUrl()
+
+        if (filterUrl.length > 0) {  // 有重複的 url ，網頁顯示對應的短網址
+          const filterShortUrl = filterUrl[0].short_url
+          res.render('index', { isSomeUrl: "true", url, shortUrl: filterShortUrl })
+        } else {                     // 沒有重複，則建立建立新的短網址
+          return urlModel.create({ url, short_url: shortUrl })
+            .then(() => res.render('show', { url, shortUrl }))
+            .catch(error => console.log(error))
+        }
+      })
+  } else {     // 不是 url 的話，網頁顯示錯誤提示訊息
+    res.render('index', { isUrl: "false" })
+  }
+})
+
+router.get("/:inputShortUrl", (req, res) => {
+  const inputShortUrl = req.params.inputShortUrl
+  
+  urlModel.findOne({ short_url: inputShortUrl })  // 在資料庫搜尋該輸入的短網址
+    .then(data => {
+      if (!data) {               // 沒有的話，網頁顯示錯誤提示訊息
+        return res.render('index', { isError: "true", inputShortUrl })
+      }
+      res.redirect(data.url)     // 有的話，則跳轉至對應的原本網址
+    })
+    .catch(error => console.error(error))
+})
+
+module.exports = router
